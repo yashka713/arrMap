e7f861a00f5fc259d042a2761ecb1ef04d570078
Yaroslav Liakh <liakh_ya@mail.ru>
2016-09-29 21:21:08 +0300

    add editable

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 /**
  * Created by Ярик on 27.09.2016.
  */
+$(document).ready(function () {
+    $.fn.editable.defaults.mode = 'popup';
+    $("#propName_1, #propData_1").editable({
+        type: 'text',
+        title: 'Enter the property'
+    }).css('left', '0');
+    $('#username').editable();
+});
