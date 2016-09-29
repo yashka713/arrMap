cd2481878f23956184f5e9e4c6ecd424ee267f25
Yaroslav Liakh <liakh_ya@mail.ru>
2016-09-29 21:39:43 +0300

    setting editable

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@
  */
 $(document).ready(function () {
     $.fn.editable.defaults.mode = 'popup';
-    $("#propName_1, #propData_1").editable({
+    $("#propName_1, #propData_1, #propName_2, #propData_2").editable({
         type: 'text',
         title: 'Enter the property'
-    }).css('left', '0');
-    $('#username').editable();
+    });
 });
